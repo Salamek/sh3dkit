778b01eb7c788d11483c577fb4b5070d8ecf70ef
Adam Schubert <adam.schubert@sg1-game.net>
2025-05-12 07:56:32 +0200

    fix(sh3d2svg): Fix double dot in file name

--- sh3dkit/bin/sh3d2svg.py
+++ sh3dkit/bin/sh3d2svg.py
@@ -33,7 +33,7 @@ def main(input_file: str, output: str, level_name: Optional[str] = None) -> None
             for level in render_levels:
                 level.is_visible = True  # Force level to be always visible
                 sanitized_level_name = re.sub(r'[^A-Za-z0-9_.-]', '_', level.name)
-                modified_path = output_path.parent.joinpath('{}_{}.{}'.format(output_path.stem, sanitized_level_name, output_path.suffix))
+                modified_path = output_path.parent.joinpath('{}_{}'.format(output_path.stem, sanitized_level_name + output_path.suffix))
                 click.echo(f'Rendering {input_file} to {modified_path}')
                 svg_home_renderer.save_to_file(modified_path, level)
         else:
